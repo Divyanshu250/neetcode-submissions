de3e40fd1c9c053c54b3dedc838edc9eb75f3d6f
Divyanshu250 <81811732+Divyanshu250@users.noreply.github.com>
2026-09-17 19:48:43 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.swift
@@ -0,0 +1,46 @@
+class Solution {
+   func sortArray(_ nums: [Int]) -> [Int] {
+       var array = nums
+       mergeSort(&array, 0, array.count - 1) 
+       return array
+    }
+
+    func mergeSort(_ nums: inout [Int],_ low: Int,_ high: Int ) {
+        if low < high {
+            let middle = (low + high) / 2
+            mergeSort(&nums, low, middle)
+            mergeSort(&nums, middle + 1, high)
+            merging(&nums, low, middle, high)
+        }
+    }
+
+    func merging(_ nums: inout [Int], _ low: Int, _ mid: Int, _ high: Int) {
+        var temp = [Int]()
+        var left = low
+        var right = mid + 1
+
+        while left <= mid && right <= high {
+            if nums[left] <= nums[right] {
+                temp.append(nums[left])
+                left += 1
+            } else {
+                temp.append(nums[right])
+                right += 1
+            }
+        }
+
+        while left <= mid {
+            temp.append(nums[left])
+            left += 1
+        }
+
+        while right <= high {
+            temp.append(nums[right])
+            right += 1
+        }
+
+        for i in 0..<temp.count {
+            nums[low + i] = temp[i]
+        }
+    }
+}
